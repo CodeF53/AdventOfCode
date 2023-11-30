bd177f7ed614af5d44b6ae1f221d25b17d804a9c
F53 <fseusb@gmail.com>
2023-11-29 17:13:26 -0700

    2022/1 as example and test of existing code

---
+++ src/2022/01.ts
@@ -0,0 +1,23 @@
+import _ from 'lodash'
+
+// returns how much calories you would get by eating the fattest elf
+export function partOne(input: string): number {
+  const elfCalories = inputToElfCalories(input)
+  const fattestElf = _.max(elfCalories)!
+
+  return fattestElf
+}
+
+// returns how much calories you would get by eating the fattest 3 elves
+export function partTwo(input: string): number {
+  const elfCalories = inputToElfCalories(input)
+
+  const elvesLeanestToFattest = elfCalories.sort()
+  const fattest3Elves = elvesLeanestToFattest.slice(-3)
+  return _.sum(fattest3Elves)
+}
+
+// sums caloric value of each elves organs, returning an array of every elf and how many calories eating them would give
+function inputToElfCalories(input: string): number[] {
+  return input.split('\n\n').map(items => _.sum(items.split('\n').map(Number)))
+}
