b00e42d498394e127be5a5b121552cdd8c28d0c9
F53 <fseusb@gmail.com>
2023-11-30 22:56:36 -0700

    improve variable names

--- src/2023/01.ts
+++ src/2023/01.ts
@@ -3,10 +3,11 @@
 export function partOne(input: string): number {
   // split input into lines, then split every line into characters, then remove every character that isn't a number
   const numbers = input.split('\n').map(entry => entry.split('').filter(Number))
+
   // take the first and last numbers of each line and turn them into a 2 digit number
-  const fuck = numbers.map(line => Number(`${line.at(0)}${line.at(-1)}`))
-  // add those fucks up
-  return _.sum(fuck)
+  const calibrationValues = numbers.map(line => Number(`${line.at(0)}${line.at(-1)}`))
+
+  return _.sum(calibrationValues)
 }
 
 const stringNums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
