a5ffbc18a432532df355124702972c563eb7fffa
F53 <fseusb@gmail.com>
2023-12-03 22:17:40 -0700

    clean p1 a bit

--- src/2023/04.ts
+++ src/2023/04.ts
@@ -6,23 +6,29 @@ interface Card {
 }
 
 // winning numbers | numbers you have
-function splitfuck(input: string): Card[] {
+function getCards(input: string): Card[] {
   const fuck = input.split('\n').map(line => line.split(': ')[1].split('|').map(section => _.reject(section.split(' '), _.isEmpty).map(Number)))
   return fuck.map(entry => ({ winningNums: entry[0], nums: entry[1] }))
 }
 
-function getCardValue({ winningNums, nums }: Card) {
-  return Math.floor(2 ** (nums.filter(num => winningNums.includes(num)).length - 1))
+function getNumWinningNumbers({ winningNums, nums }: Card) {
+  return nums.filter(num => winningNums.includes(num)).length
+}
+
+function getP1CardValue(card: Card) {
+  return Math.floor(2 ** (getNumWinningNumbers(card) - 1))
 }
 
 //
 export function partOne(input: string): number {
-  const cards: Card[] = splitfuck(input)
-  const fuck = cards.map(getCardValue)
-  return _.sum(fuck)
+  const cards: Card[] = getCards(input)
+  const cardValues = cards.map(getP1CardValue)
+  return _.sum(cardValues)
 }
 
 //
 export function partTwo(input: string): number {
+  const cards: Card[] = getCards(input)
+
   return -1
 }
