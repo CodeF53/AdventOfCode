9970a132355ac12b9dc238118999108e0ce694fe
F53 <fseusb@gmail.com>
2023-12-03 22:38:51 -0700

    2023 04 p2

--- src/2023/04.ts
+++ src/2023/04.ts
@@ -5,7 +5,6 @@ interface Card {
   nums: number[]
 }
 
-// winning numbers | numbers you have
 function getCards(input: string): Card[] {
   const fuck = input.split('\n').map(line => line.split(': ')[1].split('|').map(section => _.reject(section.split(' '), _.isEmpty).map(Number)))
   return fuck.map(entry => ({ winningNums: entry[0], nums: entry[1] }))
@@ -19,16 +18,25 @@ function getP1CardValue(card: Card) {
   return Math.floor(2 ** (getNumWinningNumbers(card) - 1))
 }
 
-//
 export function partOne(input: string): number {
   const cards: Card[] = getCards(input)
   const cardValues = cards.map(getP1CardValue)
   return _.sum(cardValues)
 }
 
-//
 export function partTwo(input: string): number {
   const cards: Card[] = getCards(input)
 
-  return -1
+  const cardWinningNumberCounts = cards.map(getNumWinningNumbers)
+
+  // determine card counts from duplication rules
+  const cardCounts = _.fill(Array(cardWinningNumberCounts.length), 1)
+  for (let i = 0; i < cardWinningNumberCounts.length - 1; i++) {
+    const winNumCount = cardWinningNumberCounts[i]
+
+    for (let c = i + 1; c < Math.min(i + winNumCount + 1, cardWinningNumberCounts.length); c++)
+      cardCounts[c] += cardCounts[i] * 1
+  }
+
+  return _.sum(cardCounts)
 }
