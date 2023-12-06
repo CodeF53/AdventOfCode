6ef6dac322929530844d0f4b7f93250de850721b
F53 <fseusb@gmail.com>
2023-12-05 23:37:27 -0700

    reduce memory impact

--- src/2023/06.ts
+++ src/2023/06.ts
@@ -6,8 +6,8 @@ interface RaceData {
   distance: number
 }
 
-function getWinningHoldTimes(raceData: RaceData): number[] {
-  const winningHoldTimes: number[] = []
+function getWinningHoldTimes(raceData: RaceData): number {
+  let numWinningTimes = 0
 
   for (let timeHeld = 1; timeHeld < raceData.time; timeHeld++) {
     const velocity = timeHeld
@@ -15,10 +15,10 @@ function getWinningHoldTimes(raceData: RaceData): number[] {
     const distance = velocity * timeRemaining
 
     if (distance > raceData.distance)
-      winningHoldTimes.push(timeHeld)
+      numWinningTimes++
   }
 
-  return winningHoldTimes
+  return numWinningTimes
 }
 
 function partialParseInput<T>(input: string, mapFn: (value: string) => T): T[] {
@@ -27,10 +27,10 @@ function partialParseInput<T>(input: string, mapFn: (value: string) => T): T[] {
 
 export function partOne(input: string): number {
   const raceDatas = _.zip(...partialParseInput(input, line => line.split(/ +/).map(Number))) as [[number, number]]
-  return arrayProduct(raceDatas.map(([time, distance]) => getWinningHoldTimes({ time, distance }).length))
+  return arrayProduct(raceDatas.map(([time, distance]) => getWinningHoldTimes({ time, distance })))
 }
 
 export function partTwo(input: string): number {
   const [time, distance] = partialParseInput(input, line => Number(line.replaceAll(' ', '')))
-  return getWinningHoldTimes({ time, distance }).length
+  return getWinningHoldTimes({ time, distance })
 }
