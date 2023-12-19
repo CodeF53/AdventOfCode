4006a37177867bfe88ff7c260fa02ba980737ad4
F53 <fseusb@gmail.com>
2023-12-18 19:38:33 -0700

    2023 18 p2

--- src/2023/18.ts
+++ src/2023/18.ts
@@ -6,114 +6,79 @@ interface Pos {
 }
 const directions = ['U', 'D', 'R', 'L'] as const
 type Direction = typeof directions[number]
-function offsetPos(origin: Pos, dir: Direction): Pos {
+function offsetPos(origin: Pos, dir: Direction, amount: number): Pos {
   switch (dir) {
-    case 'U': return { x: origin.x, y: origin.y - 1 }
-    case 'D': return { x: origin.x, y: origin.y + 1 }
-    case 'R': return { x: origin.x + 1, y: origin.y }
-    case 'L': return { x: origin.x - 1, y: origin.y }
+    case 'U': return { x: origin.x, y: origin.y - amount }
+    case 'D': return { x: origin.x, y: origin.y + amount }
+    case 'R': return { x: origin.x + amount, y: origin.y }
+    case 'L': return { x: origin.x - amount, y: origin.y }
   }
 }
 
-function createPath(input: string): { pos: Pos, dir: Direction }[] {
-  const instructions = input.split('\n').map(line => line.split(' '))
-  let currentPos = { x: 0, y: 0 }
-  const path: { pos: Pos, dir: Direction }[] = []
-  for (const instruction of instructions) {
-    const [direction, count, hexCode] = instruction
-    _.times(Number(count), () => {
-      currentPos = offsetPos(currentPos, direction as Direction)
-      path.push({ pos: currentPos, dir: direction as Direction })
-    })
-  }
-
-  return path
-}
-
-function debugLogGrid(grid: boolean[][]) {
-  console.log(
-    grid.map(line => line.map(cell => cell ? '#' : '.')
-      .join(''))
-      .join('\n'),
-  )
-}
-
-function fill(path: { pos: Pos, dir: Direction }[]): boolean[][] {
-  // get bounding box of path
-  const minX = _.minBy(path, path => path.pos.x)!.pos.x
-  const minY = _.minBy(path, path => path.pos.y)!.pos.y
-  const maxX = _.maxBy(path, path => path.pos.x)!.pos.x
-  const maxY = _.maxBy(path, path => path.pos.y)!.pos.y
-  // create 2d grid
-  const width = maxX - minX + 1
-  const height = maxY - minY + 1
-  const grid: boolean[][] = Array.from({ length: height }, () => Array(width).fill(false) as boolean[])
-  const upGrid: boolean[][] = Array.from({ length: height }, () => Array(width).fill(false) as boolean[])
-  const dnGrid: boolean[][] = Array.from({ length: height }, () => Array(width).fill(false) as boolean[])
-  // mark path as filled on the grid
-  for (const { pos, dir } of path) {
-    grid[pos.y - minY][pos.x - minX] = true
-    // mark ups and downs on grid for fill operation
-    if (dir === 'U') {
-      upGrid[pos.y - minY][pos.x - minX] = true
-      upGrid[pos.y + 1 - minY][pos.x - minX] = true
-    }
-    if (dir === 'D') {
-      dnGrid[pos.y - minY][pos.x - minX] = true
-      dnGrid[pos.y - 1 - minY][pos.x - minX] = true
-    }
-  }
-  // debugLogGrid(grid.slice(0, 10))
-
-  // fill grid
-  for (let y = 0; y < height; y++) {
-    let enclosed = false
-    let lastOp
-    for (let x = 0; x < width; x++) {
-      if (upGrid[y][x] && lastOp !== 'up') {
-        enclosed = !enclosed
-        lastOp = 'up'
-      }
-      if (dnGrid[y][x] && lastOp !== 'down') {
-        enclosed = !enclosed
-        lastOp = 'down'
-      }
-      if (enclosed)
-        grid[y][x] = true
-    }
-  }
-  // debugLogGrid(grid.slice(0, 10))
-  return grid
-}
-
-export function partOne(input: string): number {
-  const path = createPath(input)
-  const filled = fill(path)
-  return _.sumBy(filled, _.sum)
+function parseInstruction(instruction: string[]): { direction: Direction, count: number } {
+  const direction = instruction[0] as Direction
+  const count = Number(instruction[1])
+  return { direction, count }
 }
 
 function parseHexInstruction(hexCode: string): { direction: Direction, count: number } {
-  console.log(hexCode)
+  let direction: Direction
+  switch (hexCode.at(-1)) {
+    case '0':
+      direction = 'R'
+      break
+    case '1':
+      direction = 'D'
+      break
+    case '2':
+      direction = 'L'
+      break
+    case '3':
+      direction = 'U'
+      break
+    default: throw new Error(`Unexpected Direction code ${hexCode.at(-1)}`)
+  }
+  return { direction, count: Number.parseInt(hexCode.slice(0, -1), 16) }
 }
 
-function createPathP2(input: string): { pos: Pos, dir: Direction }[] {
+function createPath(input: string, hexInstructions: boolean = false): Pos[] {
   const instructions = input.split('\n').map(line => line.split(' '))
-  const currentPos = { x: 0, y: 0 }
-  const path: { pos: Pos, dir: Direction }[] = []
+  let currentPos = { x: 0, y: 0 }
+  const path: Pos[] = []
   for (const instruction of instructions) {
-    const hexCode = instruction[2].slice(2, -1)
-    const { direction, count } = parseHexInstruction(hexCode)
-    // _.times(Number(count), () => {
-    //   currentPos = offsetPos(currentPos, direction as Direction)
-    //   path.push({ pos: currentPos, dir: direction as Direction })
-    // })
+    // parse instruction
+    const { direction, count } = hexInstructions
+      ? parseHexInstruction(instruction[2].slice(2, -1))
+      : parseInstruction(instruction)
+    // go to next point
+    currentPos = offsetPos(currentPos, direction, count)
+    path.push(currentPos)
   }
 
   return path
 }
 
+// https://cp-algorithms.com/geometry/area-of-simple-polygon.html
+function getHoleSize(path: Pos[]): number {
+  let res = 0
+
+  for (let i = 0; i < path.length; i++) {
+    const p = i > 0 ? path[i - 1] : path[path.length - 1]
+    const q = path[i]
+    // area from the interior
+    res += (p.x - q.x) * (p.y + q.y)
+    // area of the border tiles
+    res += Math.abs(p.x - q.x)
+    res += Math.abs(p.y - q.y)
+  }
+  return (Math.abs(res) / 2) + 1
+}
+
+export function partOne(input: string, hexInstructions: boolean = false): number {
+  const path = createPath(input, hexInstructions)
+  return getHoleSize(path)
+}
+
 export function partTwo(input: string): number {
-  const path = createPathP2(input)
-  const filled = fill(path)
-  return _.sumBy(filled, _.sum)
+  return partOne(input, true)
 }
