5610ab5c91c3e6ccc82de724f3f18238f252e477
cassie <fseusb@gmail.com>
2024-12-08 20:23:54 -0700

    day 8

---
+++ src/2024/08.ts
@@ -0,0 +1,88 @@
+// https://adventofcode.com/2024/day/8
+
+import { type Pos, posOOB } from '../utils'
+
+function getAntiodeSets(grid: string[][]): Pos[][] {
+  const antennaGroups: Record<string, Pos[]> = {}
+  grid.forEach((line, y) => line.forEach((char, x) => {
+    if (/\w/.exec(char)) antennaGroups[char] = [...(antennaGroups[char] || []), { y, x }]
+  }))
+  return Object.values(antennaGroups)
+}
+
+function debugOutput(grid: string[][], uniqueAntiNodes: Set<string>) {
+  for (const antinode of uniqueAntiNodes) {
+    const [x, y] = antinode.split(',').map(Number)
+    grid[y][x] = '#'
+  }
+  console.log(grid.map(line => line.join('')).join('\n'))
+}
+
+export function partOne(input: string): number {
+  const grid = input.split('\n').map(line => line.split(''))
+  const uniqueAntiNodes = new Set<string>()
+  for (const antGroup of getAntiodeSets(grid)) {
+    antGroup.forEach((antA, iA) => {
+      antGroup.forEach((antB, iB) => {
+        if (iB >= iA) return
+        const offset = { x: antB.x - antA.x, y: antB.y - antA.y }
+        const offsetedPosA = { x: antA.x - offset.x, y: antA.y - offset.y }
+        const offsetedPosB = { x: antB.x + offset.x, y: antB.y + offset.y }
+        if (!posOOB(offsetedPosA, grid))
+          uniqueAntiNodes.add(`${offsetedPosA.x},${offsetedPosA.y}`)
+        if (!posOOB(offsetedPosB, grid))
+          uniqueAntiNodes.add(`${offsetedPosB.x},${offsetedPosB.y}`)
+      })
+    })
+  }
+  // debugOutput(grid, uniqueAntiNodes)
+
+  return uniqueAntiNodes.size
+}
+
+export function partTwo(input: string): number {
+  const grid = input.split('\n').map(line => line.split(''))
+  const uniqueAntiNodes = new Set<string>()
+  for (const antGroup of getAntiodeSets(grid)) {
+    antGroup.forEach((antA, iA) => {
+      antGroup.forEach((antB, iB) => {
+        if (iB === iA) return
+
+        // I have literally no idea how to solve p2 in a sane way because the logical stuff passed tests but was too low
+        const offset = { x: antB.x - antA.x, y: antB.y - antA.y }
+        let offsetedPosA = { x: antA.x - offset.x, y: antA.y - offset.y }
+        let i = 0
+        while (!posOOB(offsetedPosA, grid)) {
+          uniqueAntiNodes.add(`${offsetedPosA.x},${offsetedPosA.y}`)
+          offsetedPosA = { x: antA.x - offset.x * i, y: antA.y - offset.y * i }
+          i++
+        }
+        offsetedPosA = { x: antB.x + offset.x, y: antB.y + offset.y }
+        i = 0
+        while (!posOOB(offsetedPosA, grid)) {
+          uniqueAntiNodes.add(`${offsetedPosA.x},${offsetedPosA.y}`)
+          offsetedPosA = { x: antB.x + offset.x * i, y: antB.y + offset.y * i }
+          i++
+        }
+
+        offsetedPosA = { x: antA.x + offset.x, y: antA.y + offset.y }
+        i = 0
+        while (!posOOB(offsetedPosA, grid)) {
+          uniqueAntiNodes.add(`${offsetedPosA.x},${offsetedPosA.y}`)
+          offsetedPosA = { x: antA.x + offset.x * i, y: antA.y + offset.y * i }
+          i++
+        }
+        offsetedPosA = { x: antB.x - offset.x, y: antB.y - offset.y }
+        i = 0
+        while (!posOOB(offsetedPosA, grid)) {
+          uniqueAntiNodes.add(`${offsetedPosA.x},${offsetedPosA.y}`)
+          offsetedPosA = { x: antB.x - offset.x * i, y: antB.y - offset.y * i }
+          i++
+        }
+      })
+    })
+  }
+  // debugOutput(grid, uniqueAntiNodes)
+
+  return uniqueAntiNodes.size
+}
